db53736f45ea1d36ebe1ef7c3cd656834431548a
yuyuOuO <55227155+yuyuOuO@users.noreply.github.com>
2019-10-31 14:50:08 +0800

    20191031禁忌之獄gogogo

--- 20190917UI/Assets/Scripts/GameManager.cs
+++ 20190917UI/Assets/Scripts/GameManager.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio; //引用音頻API
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour {
     //定義欄位(宣告變數)
     //類型 名稱 結尾
     //public 公開 、private 私人
     public AudioMixer mixer;
+    public Text LoadingText;
+    public Slider Loading;
 
     //定義方法 (宣告函式)
     //修飾詞 類型 名稱(參數){敘述}
@@ -15,6 +20,12 @@ public class GameManager : MonoBehaviour {
     {
         mixer.SetFloat("VBGM", value);
     }
-
-	
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("場景");
+    }
 }
