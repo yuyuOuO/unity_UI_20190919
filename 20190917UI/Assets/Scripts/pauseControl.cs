f578452acf47f806b532b2ee983e23a787ebca47
yuyuOuO <55227155+yuyuOuO@users.noreply.github.com>
2019-12-05 14:52:38 +0800

    20191205

---
+++ 20190917UI/Assets/Scripts/pauseControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseControl : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imagePause;
+    public Sprite spritePause, spritePlay;
+    [Header("暫停")]
+    public bool pause;
+
+    private void Pause()
+    {
+        //如果 按下ESC就執行{}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("暫停~");
+            //相反 :將布林值改為相反
+            pause = !pause;
+
+            //條件運算子
+            //布林值 ?結果一：結果二
+            //布林值 true 會執行結果一
+            //布林值 salse 會執行結果二
+
+            imagePause.sprite = pause ? spritePlay : spritePause;
+
+            Time.timeScale = pause ? 0 : 1;
+        }
+        
+    }
+
+    //更新:一秒執行約60次
+    private void Update()
+    {
+        Pause();
+    }
+}
