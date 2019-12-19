967d0065d158abab7222bdd195e72490df2abf07
yuyuOuO <55227155+yuyuOuO@users.noreply.github.com>
2019-12-19 15:35:49 +0800

    20191219霸者東塔蓋起來

--- 20190917UI/Assets/Scripts/player.cs
+++ 20190917UI/Assets/Scripts/player.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class player : MonoBehaviour {
+
+    [Header("血量血條")]
     public int hp = 100;
     public Slider hpSlider;
+    [Header("食物")]
+    public Text textFood;
+    public int FoodCount;
+    public int FoodTotal;
+    [Header("時間區域")]
+    public Text textTime;
+    public float gameTime;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +26,16 @@ public class player : MonoBehaviour {
            hpSlider.value = hp;
 
         }
+        if (other.tag == "FOOD")
+        {
+            FoodCount++;
+            textFood.text = "FOOD：" + FoodCount + "/" + FoodTotal;
+            Destroy(other.gameObject);
+        }
+        if(other.name == "終點"&& FoodCount == FoodTotal)
+        {
+            print("過關");
+        }
     }
     private void OnParticleCollision(GameObject other)
     {
@@ -25,5 +45,19 @@ public class player : MonoBehaviour {
             hp -= d;
             hpSlider.value = hp;
         }
+    }
+    private void Start()
+    {
+        FoodTotal = GameObject.FindGameObjectsWithTag("FOOD").Length;
+        textFood.text = "FOOD：0/" + FoodTotal;
+    }
+    private void Update()
+    {
+        UpdateTime();
+    }
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("0.00");
     }
 }
